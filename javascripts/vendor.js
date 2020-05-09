fd19e16710b050b62f4d07bac7a85c1e5eaa7a71
unknown <chamuelin.et@gmail.com>
2020-05-09 22:12:01 +0800

    fix navbar toggle bug

--- javascripts/vendor.js
+++ javascripts/vendor.js
@@ -36,19 +36,19 @@ function sequentialImg() {
 }
 
 //
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 40) {
-            $('#topBtn').fadeIn();
-        } else {
-            $('#topBtn').fadeOut();
-        }
-    });
+// $(document).ready(function () {
+//     $(window).scroll(function () {
+//         if ($(this).scrollTop() > 40) {
+//             $('#topBtn').fadeIn();
+//         } else {
+//             $('#topBtn').fadeOut();
+//         }
+//     });
 
-    $("#topBtn").click(function () {
-        $('html ,body').animate({ scrollTop: 0 }, 800);
-    });
-});
+//     $("#topBtn").click(function () {
+//         $('html ,body').animate({ scrollTop: 0 }, 800);
+//     });
+// });
 
 //
 function myOnload() {
